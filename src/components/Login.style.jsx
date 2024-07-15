ee1005c208df01923af114a53ca49d6b898f1f47
seniiya <sasimi1244@gmail.com>
2024-07-15 15:00:02 +0900

    1주차 업로드

---
+++ src/components/Login.style.jsx
@@ -0,0 +1,14 @@
+import styled from "styled-components";
+
+export const loginpage = styled.div`
+    display; flex;
+    position: absolute;
+    width: 584px;
+    height: 532px;
+    left: calc(50% - 584px/2);
+    top: calc(50% - 532px/2 + 1.5px);
+    flex-direction: column;
+    // min-height: 100vh;
+    justify-content: space-between;
+
+`;
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,17 @@
-import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+import {BrowserRouter, Route, Routes} from "react-router-dom";
 import './index.css'
+import Login from './components/Login';
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <BrowserRouter> 
+    <>
+      {/* <Navbar> */}
+      <Routes>
+
+        <Route path="/login" element={<Login/>}/>
+      </Routes>
+    </>
+  </BrowserRouter>
 )
