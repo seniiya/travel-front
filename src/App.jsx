98170bfafb76a424df5fc8a04855c7cd32da2050
MR810 <157580088+MR810@users.noreply.github.com>
2024-08-24 08:18:31 +0900

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,9 @@
-
-
-import React, { useState } from 'react';
+// App.jsx
+import React, { useState, useEffect } from 'react';
 import { BrowserRouter as Router, Route, Routes, useLocation, useNavigate } from "react-router-dom";
 import styled from 'styled-components';
+import { AuthProvider } from './pages/Login/AuthContext.jsx';
+import axios from 'axios';
 import './index.css';
 import MainPage from './pages/MainPage.jsx';
 import Login from './pages/Login/Login.jsx';
@@ -14,7 +15,7 @@ import EmailChange from './pages/Change/EmailChange.jsx';
 import IdChange from './pages/Change/IdChange.jsx';
 import PwChange from './pages/Change/PwChange.jsx';
 import Leave from './pages/Change/Leave.jsx';
-import TravelDestinations from './pages/Destinations.jsx';
+import Destinations from './pages/Destinations.jsx';
 import PopularPage from "./pages/PopularPage.jsx";
 import TravelerRank from "./pages/TravelerRank.jsx";
 import RePW from './pages/Find/RePW.jsx';
@@ -30,7 +31,7 @@ import WritePage from './pages/Write/Write.jsx';
 import PostPage_tag from './pages/PostPage_tag.jsx';
 import PostPage_title from './pages/PostPage_title.jsx';
 import MyPage from './pages/MyPage/MyPage.jsx';
-
+import TravelBagDropdown from './Navbar/TravelBagDropdown.jsx'
 
 const AppContainer = styled.div`
   display: flex;
@@ -44,8 +45,6 @@ const ContentContainer = styled.div`
   flex-direction: column;
 `;
 
-
-
 function SearchSectionWrapper() {
   const navigate = useNavigate();
   return <SearchSection onClose={() => navigate('/')} />;
@@ -53,13 +52,14 @@ function SearchSectionWrapper() {
 
 function AppContent() {
   const [selectedDest, setSelectedDest] = useState('여행지 - 전체');
+  const [selectedCategory, setSelectedCategory] = useState('여행 가방 - 전체'); // 추가된 상태
   const location = useLocation();
   const isWritePage = location.pathname === '/write';
-  const noFooterPaths = ['/login', '/signup', '/emailsignup', '/findid', '/findpw', '/repasswd', '/write', '/emailchange', '/id-change', '/pw-change', '/leave'];
+  const noFooterPaths = ['/login', '/signup', '/emailsignup', '/findid', '/findpw', '/repasswd', '/write', '/emailchange', '/id-change', '/pw-change', '/leave', '/MyPage'];
 
   return (
     <AppContainer>
-      {!isWritePage && <Navbar setSelectedDest={setSelectedDest} />}
+      {!isWritePage && <Navbar />}
       <ContentContainer>
         <Routes>
           <Route path="/" element={<><MainPage /><Contents /></>} />
@@ -69,20 +69,23 @@ function AppContent() {
           <Route path='/findid' element={<FindIdPage />} />
           <Route path='/findpw' element={<FindPwPage />} />
           <Route path='/repasswd' element={<RePW />} />
-          <Route path='/emailchange' element={<EmailChange/>}/>
-          <Route path='/id-change' element={<IdChange/>}/>
-          <Route path='/pw-change' element={<PwChange/>}/>
-          <Route path='/leave' element={<Leave/>}/>
-          <Route path="/travel-destinations" element={<><Header selectedDest={selectedDest} setSelectedDest={setSelectedDest} /><TravelDestinations selectedDest={selectedDest} /></>} />
-          <Route path="/travel-bag" element={<div><Header2 /><TravelBag /></div>} />
-          <Route path='/traveler-rank' element={<TravelerRank/>}/>
-          <Route path='/popular-page' element={<PopularPage/>}/>
+          <Route path='/emailchange' element={<EmailChange />} />
+          <Route path='/id-change' element={<IdChange />} />
+          <Route path='/pw-change' element={<PwChange />} />
+          <Route path='/leave' element={<Leave />} />
+          <Route path="/travel-destinations" element={<><Header selectedDest={selectedDest} setSelectedDest={setSelectedDest} /><Destinations selectedDest={selectedDest} /></>} />
+          <Route path="/travel-bag" element={<>
+            <Header2 selectedCategory={selectedCategory} setSelectedCategory={setSelectedCategory} />
+            <TravelBag selectedCategory={selectedCategory} />
+          </>} />
+          <Route path='/traveler-rank' element={<TravelerRank />} />
+          <Route path='/popular-page' element={<PopularPage />} />
           <Route path="/search" element={<SearchSectionWrapper />} />
           <Route path="/music" element={<Music />} />
-          <Route path="/write" element={<WritePage/>} />
-          <Route path='/PostPage_tag' element={<PostPage_tag/>}/>
-          <Route path='/postpage_title' element={<PostPage_title/>}/> 
-          <Route path="/MyPage" element={<MyPage />} />        
+          <Route path="/write" element={<WritePage />} />
+          <Route path='/PostPage_tag' element={<PostPage_tag />} />
+          <Route path='/postpage_title' element={<PostPage_title />} />
+          <Route path="/MyPage" element={<MyPage />} />
         </Routes>
       </ContentContainer>
       {!noFooterPaths.includes(location.pathname) && <Footer />}
@@ -91,10 +94,30 @@ function AppContent() {
 }
 
 function App() {
+  useEffect(() => {
+    const token = localStorage.getItem('token');
+    if (token) {
+      axios.defaults.headers.common['Authorization'] = `Bearer ${token}`;
+    }
+
+    axios.interceptors.response.use(
+      (response) => response,
+      (error) => {
+        if (error.response.status === 401) {
+          localStorage.removeItem('token');
+          window.location.href = '/login';
+        }
+        return Promise.reject(error);
+      }
+    );
+  }, []);
+
   return (
-    <Router>
+    <AuthProvider>
+      <Router>
         <AppContent />
-    </Router>
+      </Router>
+    </AuthProvider>
   );
 }
 
